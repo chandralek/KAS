966f388ebc8db2be1710b28ff60d9eba76881015
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-25 08:26:51 +0530

    Learning bro

--- routes.tf
+++ routes.tf
@@ -6,6 +6,11 @@ resource "aws_route_table" "public-rt" {
     gateway_id = aws_internet_gateway.gw.id
   }
 
+  route {
+    cidr_block = data.aws_vpc.management.cidr_block
+    gateway_id = aws_vpc_peering_connection.foo.id
+  }
+
   tags = {
     Name = "public-rt"
   }
@@ -23,6 +28,11 @@ resource "aws_route_table" "private-rt" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.gw.id
+  }
+
+  route {
+    cidr_block = data.aws_vpc.management.cidr_block
+    gateway_id = aws_vpc_peering_connection.foo.id
   }
 
   tags = {
