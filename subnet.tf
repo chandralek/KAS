a6094f430328a5054e9544c547de7f88993a996c
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-25 07:16:16 +0530

    Learning bro

--- subnet.tf
+++ subnet.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "private-subnet" {
   count             = length(data.aws_availability_zones.available.names)
   vpc_id            = aws_vpc.main.id
   availability_zone = element(data.aws_availability_zones.available.names,count.index)
-  cidr_block        = cidrsubnet(var.CIDR_BLOCK_VPC, 8, count.index)
+  cidr_block        = cidrsubnet(var.CIDR_BLOCK_VPC, 8, count.index+length(data.aws_availability_zones.available.names))
 
   tags = {
     Name            = "private-subnet-${count.index}"
