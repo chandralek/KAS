1c8f1b1f3488cddaf701cf639d35668918870799
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-25 07:11:19 +0530

    Learning bro

--- subnet.tf
+++ subnet.tf
@@ -8,3 +8,14 @@ resource "aws_subnet" "public-subnet" {
     Name            = "public-subnet-${count.index}"
   }
 }
+
+resource "aws_subnet" "private-subnet" {
+  count             = length(data.aws_availability_zones.available.names)
+  vpc_id            = aws_vpc.main.id
+  availability_zone = element(data.aws_availability_zones.available.names,count.index)
+  cidr_block        = cidrsubnet(var.CIDR_BLOCK_VPC, 8, count.index)
+
+  tags = {
+    Name            = "private-subnet-${count.index}"
+  }
+}
