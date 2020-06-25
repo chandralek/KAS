fe4feaadcabc8843e3610bc1a97a171ccaddaf85
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-25 07:37:12 +0530

    Learning bro

--- routes.tf
+++ routes.tf
@@ -16,3 +16,22 @@ resource "aws_route_table_association" "a" {
   subnet_id      = element(tolist(aws_subnet.public-subnet.*.id),count.index )
   route_table_id = aws_route_table.public-rt.id
 }
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = length(tolist(aws_subnet.private-subnet.*.id))
+  subnet_id      = element(tolist(aws_subnet.private-subnet.*.id),count.index )
+  route_table_id = aws_route_table.public-rt.id
+}
