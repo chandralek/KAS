c777b525adc1923b8bc893a654017baf307fa997
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-25 07:28:57 +0530

    Learning bro

--- routes.tf
+++ routes.tf
@@ -10,3 +10,8 @@ resource "aws_route_table" "public-rt" {
     Name = "public-rt"
   }
 }
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public-subnet.*.id
+  route_table_id = aws_route_table.public-rt.id
+}
