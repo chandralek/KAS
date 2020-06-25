9c417a234fe3ead7bc15a9a90d8e6fcfe628ca40
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-25 08:23:08 +0530

    Learning bro

--- peering.tf
+++ peering.tf
@@ -1,13 +1,6 @@
 resource "aws_vpc_peering_connection" "foo" {
   peer_vpc_id   = data.aws_vpc.management.id
   vpc_id        = aws_vpc.main.id
-
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
+  auto_accept   = true
 }
 
