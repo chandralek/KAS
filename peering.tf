ac7a49731181e3179a859bab13124d94aa6cfd24
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-27 16:19:32 +0530

    Learning bro

--- peering.tf
+++ peering.tf
@@ -4,3 +4,13 @@ resource "aws_vpc_peering_connection" "foo" {
   auto_accept   = true
 }
 
+data "aws_route_tables" "managementtable"{
+  vpc_id = data.aws_vpc.management.id
+}
+
+resource "aws_route" "route" {
+  count                     = length(tolist(data.aws_route_tables.managementtable.ids))
+  route_table_id            = element(tolist(data.aws_route_tables.managementtable.ids), count.index)
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+}
