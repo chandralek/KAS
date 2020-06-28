ddc581af1e381517c77ff008200843d58cb2a2be
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-28 16:24:44 +0530

    Learning bro

--- Mysql/main.tf
+++ Mysql/main.tf
@@ -10,6 +10,7 @@ resource "aws_db_instance" "default" {
   parameter_group_name = aws_db_parameter_group.mysql-db-parameter.name
   db_subnet_group_name = aws_db_subnet_group.mysql-subnet-group.name
   vpc_security_group_ids = [aws_security_group.allow_mysql.id]
+  identifier            = "mysql-nonprod"
 }
 
 data aws_vpc "vpc"{
