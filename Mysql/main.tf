c58f82209625f86abcf508873c3896b023a1b1c6
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-28 16:01:46 +0530

    Learning bro

--- Mysql/main.tf
+++ Mysql/main.tf
@@ -9,6 +9,35 @@ resource "aws_db_instance" "default" {
   password             = "DevOps321"
   parameter_group_name = aws_db_parameter_group.mysql-db-parameter.name
   db_subnet_group_name = aws_db_subnet_group.mysql-subnet-group.name
+  vpc_security_group_ids = [aws_security_group.allow_mysql.id]
+}
+
+data aws_vpc "vpc"{
+  id = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_security_group" "allow_mysql" {
+  name          = "allow_mysql"
+  vpc_id        = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_mysql"
+  }
 }
 
 resource "aws_db_parameter_group" "mysql-db-parameter" {
@@ -18,3 +47,4 @@ resource "aws_db_parameter_group" "mysql-db-parameter" {
 resource "aws_db_subnet_group" "mysql-subnet-group" {
   subnet_ids = tolist(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS)
 }
+
