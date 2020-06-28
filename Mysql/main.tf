9cd64e031cea867802ecbbeb615ba54dac4f6004
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-28 12:58:16 +0530

    Learning bro

---
+++ Mysql/main.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.6"
+  instance_class       = "db.t2.micro"
+  name                 = "mysql-nonprod"
+  username             = "admin"
+  password             = "admin"
+  parameter_group_name = aws_db_parameter_group.mysql-db-parameter.name
+  db_subnet_group_name = aws_db_subnet_group.mysql-subnet-group.name
+}
+
+resource "aws_db_parameter_group" "mysql-db-parameter" {
+  family = "mysql5.6"
+}
+
+resource "aws_db_subnet_group" "mysql-subnet-group" {
+  subnet_ids = [tolist(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS)]
+}
---
+++ Mysql/state.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstates-demo"
+    key    = "prod/mysql/terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "terraform-locking"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "tfstates-demo"
+    key    = "prod/vpc/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
