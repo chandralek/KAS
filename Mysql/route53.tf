8bcf8eefc38d9d50166f168f59a430b9aee68107
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-28 16:06:13 +0530

    Learning bro

--- Mysql/route53.tf
+++ Mysql/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
   zone_id = "Z06359783V969HYMH8KRR"
   name    = "mysql-nonprod.devops46.online"
-  type    = "A"
+  type    = "CNAME"
   ttl     = "5"
   records = [aws_db_instance.default.address]
 }
